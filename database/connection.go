6575702b7b2af4626bd3abf65766c8b209217048
arifinoid <rohmadarifin007@gmail.com>
2021-02-28 18:44:41 +0700

    Initial commit

---
+++ database/connection.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"../models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("root:root@/go_auth"), &gorm.Config{})
+	if err != nil {
+		panic("Couldn't connect to database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
